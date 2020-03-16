f4a28323b38c176b2bf3667182a7a5a26ee1d581
Megan Penn <penn.meg@gmail.com>
2020-03-16 10:35:47 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #838383;
+`;
+
+const Card = styled.div`
+  width: 300px;
+  height: 450px;
+  background-color: white;
+  border-radius: 20px;
+`;
+
+const Top = styled.div`
+  background-color: #838383;
+  color: white;
+`;
+
+const Bottom = styled.div`
+  color: #838383;
+  background-color: white;
+`;
+
+// inspo - https://www.uplabs.com/posts/minimal-weather-app-8bea15b9-540a-4667-a584-053897e2f7ca
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title>Weather App</Title>
       </header>
+      <div>
+        <Card>
+          <Top />
+          <Bottom>
+          <p>Temperature</p>
+          <p>City</p>
+          <p>Date & Time </p>
+          </Bottom>
+        </Card>
+      </div>
     </div>
   );
 }
